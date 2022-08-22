ccd7b38c7d1ae332859bcaa52a22ea587e278aac
MacLier <75802094+MacLier@users.noreply.github.com>
2022-08-22 17:27:51 +0200

    feat: CORS access middleware added

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST,PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-types, Authorization');
+    next();
+});
+
 app.use(bodyParser.json());
 app.use('/feed', feedRoutes);
 app.listen(3000);
