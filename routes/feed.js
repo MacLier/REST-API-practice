e233e65246a8c363475198e0b9680aa0a1c52a75
MacLier <75802094+MacLier@users.noreply.github.com>
2022-08-22 17:04:11 +0200

    feat: feed POST api created

--- routes/feed.js
+++ routes/feed.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const feedController = require('../controllers/feed');
 
 router.get('/posts', feedController.getPosts);
+router.post('/post', feedController.createPost);
 
 module.exports = router;
